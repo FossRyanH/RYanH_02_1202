47cad034c59b29ebeeff9dcd000feae9fd31ef01
Ryan Hinds <fossryanh@gmail.com>
2024-03-27 01:05:19 -0600

    Made some Materials for Projectile

--- Assi02_RyanH_1202/Intermediate/Build/Win64/UnrealEditor/Inc/Assi02_RyanH_1202/UHT/Assi02_RyanH_1202.init.gen.cpp
+++ Assi02_RyanH_1202/Intermediate/Build/Win64/UnrealEditor/Inc/Assi02_RyanH_1202/UHT/Assi02_RyanH_1202.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeAssi02_RyanH_1202_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x627F51F2,
+				0xE5343166,
 				0x46E6E6AF,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeAssi02_RyanH_1202_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_Assi02_RyanH_1202.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Assi02_RyanH_1202(Z_Construct_UPackage__Script_Assi02_RyanH_1202, TEXT("/Script/Assi02_RyanH_1202"), Z_Registration_Info_UPackage__Script_Assi02_RyanH_1202, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x627F51F2, 0x46E6E6AF));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Assi02_RyanH_1202(Z_Construct_UPackage__Script_Assi02_RyanH_1202, TEXT("/Script/Assi02_RyanH_1202"), Z_Registration_Info_UPackage__Script_Assi02_RyanH_1202, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xE5343166, 0x46E6E6AF));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Assi02_RyanH_1202/Intermediate/Build/Win64/UnrealEditor/Inc/Assi02_RyanH_1202/UHT/Projectile.gen.cpp
+++ Assi02_RyanH_1202/Intermediate/Build/Win64/UnrealEditor/Inc/Assi02_RyanH_1202/UHT/Projectile.gen.cpp
@@ -12,6 +12,7 @@ void EmptyLinkFunctionForGeneratedCodeProjectile() {}
 	ASSI02_RYANH_1202_API UClass* Z_Construct_UClass_AProjectile();
 	ASSI02_RYANH_1202_API UClass* Z_Construct_UClass_AProjectile_NoRegister();
 	ENGINE_API UClass* Z_Construct_UClass_AActor();
+	ENGINE_API UClass* Z_Construct_UClass_UStaticMeshComponent_NoRegister();
 	UPackage* Z_Construct_UPackage__Script_Assi02_RyanH_1202();
 // End Cross Module References
 	void AProjectile::StaticRegisterNativesAProjectile()
@@ -28,6 +29,11 @@ void EmptyLinkFunctionForGeneratedCodeProjectile() {}
 #if WITH_METADATA
 		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
 #endif
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_ProjectileMesh_MetaData[];
+#endif
+		static const UECodeGen_Private::FObjectPropertyParams NewProp_ProjectileMesh;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
 		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
 		static const UECodeGen_Private::FClassParams ClassParams;
 	};
@@ -42,6 +48,17 @@ void EmptyLinkFunctionForGeneratedCodeProjectile() {}
 		{ "ModuleRelativePath", "Public/Projectile.h" },
 	};
 #endif
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AProjectile_Statics::NewProp_ProjectileMesh_MetaData[] = {
+		{ "Category", "Components" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "Public/Projectile.h" },
+	};
+#endif
+	const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AProjectile_Statics::NewProp_ProjectileMesh = { "ProjectileMesh", nullptr, (EPropertyFlags)0x0040000000090009, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AProjectile, ProjectileMesh), Z_Construct_UClass_UStaticMeshComponent_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AProjectile_Statics::NewProp_ProjectileMesh_MetaData), Z_Construct_UClass_AProjectile_Statics::NewProp_ProjectileMesh_MetaData) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_AProjectile_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AProjectile_Statics::NewProp_ProjectileMesh,
+	};
 	const FCppClassTypeInfoStatic Z_Construct_UClass_AProjectile_Statics::StaticCppClassTypeInfo = {
 		TCppClassTypeTraits<AProjectile>::IsAbstract,
 	};
@@ -51,15 +68,16 @@ void EmptyLinkFunctionForGeneratedCodeProjectile() {}
 		&StaticCppClassTypeInfo,
 		DependentSingletons,
 		nullptr,
-		nullptr,
+		Z_Construct_UClass_AProjectile_Statics::PropPointers,
 		nullptr,
 		UE_ARRAY_COUNT(DependentSingletons),
 		0,
-		0,
+		UE_ARRAY_COUNT(Z_Construct_UClass_AProjectile_Statics::PropPointers),
 		0,
 		0x009000A4u,
 		METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AProjectile_Statics::Class_MetaDataParams), Z_Construct_UClass_AProjectile_Statics::Class_MetaDataParams)
 	};
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AProjectile_Statics::PropPointers) < 2048);
 	UClass* Z_Construct_UClass_AProjectile()
 	{
 		if (!Z_Registration_Info_UClass_AProjectile.OuterSingleton)
@@ -79,9 +97,9 @@ void EmptyLinkFunctionForGeneratedCodeProjectile() {}
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
 	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Assi02_RyanH_1202_Source_Assi02_RyanH_1202_Public_Projectile_h_Statics::ClassInfo[] = {
-		{ Z_Construct_UClass_AProjectile, AProjectile::StaticClass, TEXT("AProjectile"), &Z_Registration_Info_UClass_AProjectile, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AProjectile), 984595414U) },
+		{ Z_Construct_UClass_AProjectile, AProjectile::StaticClass, TEXT("AProjectile"), &Z_Registration_Info_UClass_AProjectile, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AProjectile), 3303319270U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Assi02_RyanH_1202_Source_Assi02_RyanH_1202_Public_Projectile_h_225231524(TEXT("/Script/Assi02_RyanH_1202"),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Assi02_RyanH_1202_Source_Assi02_RyanH_1202_Public_Projectile_h_375175821(TEXT("/Script/Assi02_RyanH_1202"),
 		Z_CompiledInDeferFile_FID_Assi02_RyanH_1202_Source_Assi02_RyanH_1202_Public_Projectile_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Assi02_RyanH_1202_Source_Assi02_RyanH_1202_Public_Projectile_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
